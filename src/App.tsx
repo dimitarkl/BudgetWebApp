0064db36045fd54d75cd7b8e0aab453228a80801
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-10-20 18:56:45 +0300

    Add User Authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,33 @@ import Home from '@/components/home/Home'
 import { Header } from './components/header/Header'
 import { ThemeProvider } from "@/components/ui/theme-provider"
 import Login from './components/login/Login'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import Register from './components/register/Register'
+import UserContext from './components/contexts/UserContext'
+import { auth } from './lib/firebase'
+import { useEffect, useState } from 'react'
 function App() {
+  const [loggedIn, setLoggedIn] = useState(auth.currentUser);
+  const navigate = useNavigate()
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => setLoggedIn(user));
+    return () => unsubscribe();
+  }, [])
   return (
+    <UserContext.Provider value={loggedIn}>
+      <ThemeProvider defaultTheme="dark" >
+        <Header />
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={< Login />} />
+            <Route path='/register' element={< Register />} />
+          </Routes>
+        </main>
 
-    <ThemeProvider defaultTheme="dark" >
-      <Header />
-      <main>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={< Login />} />
-          <Route path='/register' element={< Register />} />
-        </Routes>
-      </main>
-
-    </ThemeProvider>
+      </ThemeProvider>
+    </UserContext.Provider>
   )
 }
 
