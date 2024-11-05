b7c980dd5e976cb5a3aa66443a6c3830d4c27c64
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-11-05 18:43:02 +0200

    Update Error Style

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -14,12 +14,16 @@ export function Error({
     message
 }: Props) {
     return (
-        <Alert variant="destructive">
-            <AlertCircle className="h-4 w-4" />
-            <AlertTitle>Error</AlertTitle>
-            <AlertDescription>
-                {message}
-            </AlertDescription>
-        </Alert>
+
+        <div className="absolute top-16 left-1/2 transform -translate-x-1/2">
+            <Alert variant="destructive">
+                < AlertCircle className="h-4 w-4" />
+                <AlertTitle className="font-bold text-xl">Error</AlertTitle>
+                <AlertDescription >
+                    {message}
+                </AlertDescription>
+            </Alert >
+        </div >
+
     )
 }
