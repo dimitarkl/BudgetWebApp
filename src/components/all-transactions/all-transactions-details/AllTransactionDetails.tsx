cbac3b11d430c07c5bd128afd052e370b40d966b
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-11-08 12:26:50 +0200

    Rewrite Details Dialog

--- src/components/all-transactions/all-transactions-details/AllTransactionDetails.tsx
+++ src/components/all-transactions/all-transactions-details/AllTransactionDetails.tsx
@@ -7,7 +7,7 @@ import {
     DialogTitle,
     DialogTrigger
 } from "@/components/ui/dialog";
-import { Edit, Info } from "lucide-react";
+import { Edit } from "lucide-react";
 import DeleteTransaction from "./delete-transaction/DeleteTransaction";
 import ExpenseEntry from "@/components/expense-entry/ExpenseEntry";
 
@@ -43,56 +43,48 @@ export default function AllTransactionDetails(
         description: expense.description
     }
     return (
-        <>
-            <Dialog>
-                <DialogTrigger asChild>
-                    <Button variant="ghost" size="icon">
-                        <Info className="h-4 w-4" />
-                        <span className="sr-only">View details</span>
-                    </Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px] ">
-                    <DialogHeader>
-                        <DialogTitle>Transaction Details</DialogTitle>
-                        <DialogDescription >
-                            Full information about this expense.
-                        </DialogDescription>
-                    </DialogHeader>
-                    <div className="grid gap-4 py-4">
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <span className="font-bold">Date:</span>
-                            <span className="col-span-3">{expense.createdAt}</span>
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <span className="font-bold">Category:</span>
-                            <span className="col-span-3">{expense.type.charAt(0).toUpperCase() + expense.type.slice(1)}</span>
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <span className="font-bold">Amount:</span>
-                            <span className="col-span-3">{currency}{expense.sum.toFixed(2)}</span>
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <span className="font-bold">Description:</span>
-                            <span className="col-span-3 ml-2 truncate">{expense.description}</span>
-                        </div>
-                    </div>
-                    <div className="flex justify-end space-x-4 mt-6">
-                        <DeleteTransaction id={expense.id} />
-                        <Dialog>
-                            <DialogTrigger asChild>
-                                <Button
-                                    variant="default"
-                                    className="w-full sm:w-auto"
-                                >
-                                    <Edit className="w-4 h-4 mr-2" />
-                                    Edit
-                                </Button>
-                            </DialogTrigger>
-                            <ExpenseEntry expense={transaction} inputType="Edit" />
-                        </Dialog>
-                    </div>
-                </DialogContent>
-            </Dialog >
-        </>
+
+        <DialogContent className="sm:max-w-[425px] ">
+            <DialogHeader>
+                <DialogTitle>Transaction Details</DialogTitle>
+                <DialogDescription >
+                    Full information about this expense.
+                </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <span className="font-bold">Date:</span>
+                    <span className="col-span-3">{expense.createdAt}</span>
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <span className="font-bold">Category:</span>
+                    <span className="col-span-3">{expense.type.charAt(0).toUpperCase() + expense.type.slice(1)}</span>
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <span className="font-bold">Amount:</span>
+                    <span className="col-span-3">{currency}{expense.sum.toFixed(2)}</span>
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <span className="font-bold">Description:</span>
+                    <span className="col-span-3 ml-2 truncate">{expense.description}</span>
+                </div>
+            </div>
+            <div className="flex justify-end space-x-4 mt-6">
+                <DeleteTransaction id={expense.id} />
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button
+                            variant="default"
+                            className="w-full sm:w-auto"
+                        >
+                            <Edit className="w-4 h-4 mr-2" />
+                            Edit
+                        </Button>
+                    </DialogTrigger>
+                    <ExpenseEntry expense={transaction} inputType="Edit" />
+                </Dialog>
+            </div>
+        </DialogContent>
+
     )
 }
